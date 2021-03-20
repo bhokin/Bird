ad909c7e4f8e4f8d8efea12458ee66519e59d4cf
Bhokin Watanapitak <zbeamz@hotmail.com>
2021-03-20 12:40:51 +0700

    complete collision and add more pillar

--- flappydot.py
+++ flappydot.py
@@ -12,6 +12,7 @@ GRAVITY = 2.5
 STARTING_VELOCITY = -30
 PILLAR_SPEED = 6
 JUMP_VELOCITY = -20
+POSITION_PILLAR_PAIR_2 = random.randint(100, 400)
 
 
 class Dot(Sprite):
@@ -52,6 +53,11 @@ class PillarPair(Sprite):
             return True
         return False
 
+    def is_collision(self, other):
+        if self.x - 45 <= other.x <= self.x + 45:
+            if other.y <= self.y - 95 or self.y + 95 <= other.y:
+                return True
+
     def reset_position(self):
         self.x = CANVAS_WIDTH + 40
 
@@ -86,7 +92,9 @@ class FlappyGame(GameApp):
         self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
         self.elements.append(self.dot)
         self.pillar_pair = PillarPair(self, 'images/pillar-pair.png', CANVAS_WIDTH, CANVAS_HEIGHT // 2)
+        self.pillar_pair_2 = PillarPair(self, 'images/pillar-pair.png', CANVAS_WIDTH*1.54, POSITION_PILLAR_PAIR_2)
         self.elements.append(self.pillar_pair)
+        self.elements.append(self.pillar_pair_2)
 
     def init_game(self):
         self.create_sprites()
@@ -98,19 +106,21 @@ class FlappyGame(GameApp):
         if self.pillar_pair.is_out_of_screen():
             self.pillar_pair.reset_position()
             self.pillar_pair.random_height()
+        if self.pillar_pair_2.is_out_of_screen():
+            self.pillar_pair_2.reset_position()
+            self.pillar_pair_2.random_height()
         if self.background.is_out_of_screen():
             self.background.reset_position()
-        if self.dot.is_out_of_screen():
+        if self.dot.is_out_of_screen() or self.pillar_pair.is_collision(self.dot)\
+                or self.pillar_pair_2.is_collision(self.dot):
             messagebox.showinfo(title="Flappy Dot Game", message="Boommmmmmmmm!")
             root.destroy()
-            self.dot.is_started = False
-            self.pillar_pair.is_started = False
-            self.background.is_started = False
 
     def on_key_pressed(self, event):
         self.dot.start()
         self.dot.jump()
         self.pillar_pair.start()
+        self.pillar_pair_2.start()
         self.background.start()
 
 
